19ba48643edb1560c7a191c903280cb282bf0105
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2023-09-09 19:24:34 +0400

    fix hw05

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate, useNavigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -12,11 +12,6 @@ export const PATH = {
 }
 
 function Pages() {
-    const navigate = useNavigate();
-
-    React.useEffect(() => {
-        navigate(PATH.PRE_JUNIOR)
-    }, [])
 
     return (
         <div>
@@ -24,7 +19,7 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                <Route index path='/' />
+                <Route index path='/' element={<Navigate to={PATH.PRE_JUNIOR}/>} />
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>} />
