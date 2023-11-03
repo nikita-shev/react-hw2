c88355cfafc2e07c401d7c34a3b98c22a0ea0039
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2023-11-03 19:12:10 +0400

    add hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -3,28 +3,33 @@ import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
 import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
+import s from './HW10.module.css'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
 
 /*
-* 1 - в файле loadingReducer.ts дописать типы и логику
-* 2 - получить isLoading из редакса
-* 3 - дописать функцию setLoading
-* 4 - сделать стили в соответствии с дизайном
+* [x] - в файле loadingReducer.ts дописать типы и логику
+* [x] - получить isLoading из редакса
+* [x] - дописать функцию setLoading
+* [x] - сделать стили в соответствии с дизайном
 * */
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType>((state) => state.loading.isLoading);
+    const dispatch = useDispatch();
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
+        dispatch(loadingAC(true))
         // dispatch
-
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
         // setTimeout
     }
 
     return (
-        <div id={'hw10'}>
+        <div id={'hw10'} className={s.hw10}>
             <div className={s2.hwTitle}>Homework #10</div>
 
             <div className={s2.hw}>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,9 +2,10 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ActionType): StateType => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
+        case "CHANGE_LOADING":
+            return { isLoading: action.isLoading }
 
         default:
             return state
@@ -20,3 +21,7 @@ export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
 })
+
+type ActionType = LoadingActionType;
+
+type StateType = { isLoading: boolean }
